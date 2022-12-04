bdecb8307af061a9952271c9275b373000b185b4
BM-K <klbm126@khu.ac.kr>
2022-12-04 21:06:54 +0900

    Update parser.py

--- QuestionDifficultyEstimation/data/DramaQA_v2.1/AnotherMissOh/parser.py
+++ QuestionDifficultyEstimation/data/DramaQA_v2.1/AnotherMissOh/parser.py
@@ -64,29 +64,6 @@ if __name__=="__main__":
                         utterance += contain["utter"].replace('\n','')
                 except KeyError:
                     pass
-
-            #extract description
-            #description = ''
-           
-            #if vid[-4:] == "0000":
-            #    for shot_num in shot_contained_number:
-            #        length = len(str(shot_num))
-            #        if length == 1:
-            #            shot_id = vid[:-4]+'000'+str(shot_num)
-            #        elif length == 2:
-            #            shot_id = vid[:-4]+'00'+str(shot_num)
-            #        elif length == 3:
-            #            shot_id = vid[:-4]+'0'+str(shot_num)
-            #        elif length == 4:
-            #            shot_id = vid[:-4]+str(shot_num)
-            #        else:
-            #            shot_id = None
-            #        
-            #        for text in description_data[shot_id]:
-            #            description += text.replace('\n','') 
-            #else:
-            #for text in description_data[vid]:
-            #    description += text.replace('\n','')
     
             inform.append([vid, qid, que, answers, utterance, q_level_mem, q_level_logic, qa_level, shot_contained_number])
             qa_level_set.add(q_level_mem)
